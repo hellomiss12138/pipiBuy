07d5801db912b616fe92add3573282c2efebaf84
hellomiss12138 <791035126@qq.com>
2018-11-22 21:12:24 +0800

    首页右侧侧边栏数据获取

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+/* 引入样式文件 */
+import './assets/css/style.css';
+/* 引入路由 */
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+/* 导入组件 */
+import index from './components/index.vue';
+
+/* 路由规则 */
+let routes = [{
+  path: '/',
+  redirect: '/index',
+}, {
+  path: '/index',
+  component: index,
+}, ]
+
+/* 创建路由实例 */
+let router = new VueRouter({
+  routes
+})
+
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router
+})
